4d649b8a45aa91a34e614df5795e9daf12e56b6c
whuck <70232597+whuck@users.noreply.github.com>
2020-08-25 16:50:47 -0500

    first
    week1 tickets csv program

---
+++ Week1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace WillHuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main Menu / Screen
+            string menuInput = "";
+            string file = "Tickets.csv";
+            Console.WriteLine("Welcome to TicketFest!");
+            while (menuInput != "0") {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("[1]Display Tickets");
+                Console.WriteLine("[2]Create Ticket");
+                Console.WriteLine("[0]Quit");
+                //grab input
+                menuInput = Console.ReadLine();
+                //Display tickets... ie read file and display data
+                if(menuInput == "1") {
+                    //makesure the file exists/can be read
+                    if(System.IO.File.Exists(file)) {
+                        StreamReader sr = new StreamReader(file);
+                        Console.WriteLine("----------------------------------Tickets----------------------------------");
+                        //grab file data line by line and display it
+                        while(!sr.EndOfStream) {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                        sr.Close();
+                        Console.WriteLine("---------------------------------------------------------------------------");
+                    }
+                }
+                else if(menuInput =="2"){
+                    if(System.IO.File.Exists(file)) {
+                        StreamWriter sw = new StreamWriter(file, append: true);
+                        string input = "";
+                        Console.WriteLine("Ticket ID:");
+                        input += Console.ReadLine();
+                        Console.WriteLine("Summary:");
+                        input += ',' + Console.ReadLine();
+                        Console.WriteLine("Status:");
+                        input += ',' + Console.ReadLine();
+                        Console.WriteLine("Priority:");
+                        input += ',' + Console.ReadLine();
+                        Console.WriteLine("Submitter:");
+                        input += ',' + Console.ReadLine();
+                        Console.WriteLine("Assigned:");
+                        input += ',' + Console.ReadLine();
+                        Console.WriteLine("Watching:");
+                        input += ',' + Console.ReadLine();
+                        sw.WriteLine(input);
+                        sw.Close();
+                    }
+                }
+            }
+        }
+    }
+}
