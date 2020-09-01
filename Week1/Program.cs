a2ab05a8814a6c79826b8344d326f089b77774ad
whuck <70232597+whuck@users.noreply.github.com>
2020-09-01 15:20:45 -0500

    added file creation & some more comments

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -11,6 +11,8 @@ namespace WillHuck
             string menuInput = "";
             string file = "Tickets.csv";
             Console.WriteLine("Welcome to TicketFest!");
+
+            //loop menu options until quit is selected
             while (menuInput != "0") {
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("[1]Display Tickets");
@@ -18,22 +20,32 @@ namespace WillHuck
                 Console.WriteLine("[0]Quit");
                 //grab input
                 menuInput = Console.ReadLine();
+
                 //Display tickets... ie read file and display data
                 if(menuInput == "1") {
-                    //makesure the file exists/can be read
+                    //look for file if its not there create it
                     if(System.IO.File.Exists(file)) {
                         StreamReader sr = new StreamReader(file);
-                        Console.WriteLine("----------------------------------Tickets----------------------------------");
+                        Console.WriteLine($"___________________________{file}____________________________________________________________");
                         //grab file data line by line and display it
                         while(!sr.EndOfStream) {
                             string line = sr.ReadLine();
                             Console.WriteLine(line);
                         }
                         sr.Close();
-                        Console.WriteLine("---------------------------------------------------------------------------");
+                        Console.WriteLine("__________________________________________________________________________________________");
+                    }
+                    else { //create file
+                        Console.WriteLine("Ticket file not found, creating /Tickets.csv");
+                        StreamWriter sw = new StreamWriter(file);
+                        sw.WriteLine("0,Summary,Status,Priority,Submitter,Assigned,Watching1|Watching2");
+                        sw.Close();
+
                     }
                 }
+                //create ticket line in file
                 else if(menuInput =="2"){
+                    //ask for each ticket field then smush em together and write to file
                     if(System.IO.File.Exists(file)) {
                         StreamWriter sw = new StreamWriter(file, append: true);
                         string input = "";
@@ -54,7 +66,7 @@ namespace WillHuck
                         sw.WriteLine(input);
                         sw.Close();
                     }
-                }
+                }                
             }
         }
     }
